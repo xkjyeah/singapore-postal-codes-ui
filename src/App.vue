c3682449f73199f1db458781723c768d7525ecdd
Daniel Sim <daniel.ssq89@gmail.com>
2017-06-07 21:24:20 +0800

    iinitial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="search-box">
+    </div>
+    <div class="search-results">
+      <div class="search-list">
+      </div>
+      <gmap-map class="search-results">
+      </gmap-map>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
